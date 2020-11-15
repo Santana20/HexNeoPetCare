78651792d9dc37d6c846b7fb158c339c6f6d4a99
Computer <sstfmscontreras@gmail.com>
2020-11-14 22:20:02 -0500

    Sonarqube agregado

--- testing.sh
+++ testing.sh
@@ -6,3 +6,10 @@ echo "Ejecuntando pruebas unitarias"
 mvn test
 echo "Ejecuntando Jacoco report"
 mvn jacoco:report
+echo "Ejecutando testing calidad sonarqube"
+# http://sonarqube:9000 (Docker for windows, Docker for MAC o Docker Linux)
+# http://192.168.99.101:9000 (Docker for toolbox) Usa la IP!!!
+mvn sonar:sonar \
+  -Dsonar.projectKey=hexneopetcare \
+  -Dsonar.host.url=http://192.168.99.100:9000 \
+  -Dsonar.login=5b4aa1d1a005af0ae617268868124eb9bbaee73a
